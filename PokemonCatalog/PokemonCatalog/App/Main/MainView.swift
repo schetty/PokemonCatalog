0e771e6ec586bc779fb3bbf99879acff065f7516
Naomi Schettini <naomischettini@gmail.com>
2024-04-03 16:56:02 +0200

    Add folders for existing default screens.

---
+++ PokemonCatalog/PokemonCatalog/App/Main/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonCatalog
+//
+//  Created by User on 4/3/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ PokemonCatalog/PokemonCatalog/App/PokemonCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonCatalogApp.swift
+//  PokemonCatalog
+//
+//  Created by User on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
